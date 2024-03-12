f2ba28d32d5b92c3ccdd003d4128df01b0a4c3c1
GKulawik <grzegorz.kulawik@rockwellautomation.com>
2024-03-12 13:23:27 +0100

    Add fixtures to unit tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -4,32 +4,41 @@ from LibraryApp.src.library import Library, Book
 
 class TestLibrary:
 
-    def test_add_book_resource(self):
-        """
-        Adding a Book resource
-        """
+    @pytest.fixture
+    def setup_data(self):
         self.library1 = Library()
         self.book1 = Book(title="The Lord of the Rings - Trilogy",
                           genre="fantasy fiction",
-                          availability_status=False,
                           author="J.R.R. Tolkien",
                           isbn="978-0261103252")
+        return self.library1, self.book1
 
-        self.library1.add_resource(self.book1)
+    def test_add_book_resource(self, setup_data):
+        """
+        Adding 4 books
+        """
+        self.library1.add_resource(self.book1, 4)
         assert self.library1.resources == [Book(title='The Lord of the Rings - Trilogy',
                                                 genre='fantasy fiction',
-                                                availability_status=False,
+                                                availability_status=True,
+                                                quantity=4,
                                                 author='J.R.R. Tolkien',
                                                 isbn='978-0261103252')]
 
-    def test_add_unsupported_resource(self):
+    def test_add_unsupported_resource(self, setup_data):
         """
-       Adding a resource of not supported type, e.g., a list
+       Adding a resource of not supported type, e.g., a list, should throw a TypeError
        """
-        self.library1 = Library()
-        self.book1 = ["Book"]
+        self.book_of_list_type = ["Book"]
         with pytest.raises(TypeError):
-            self.library1.add_resource(self.book1)
+            self.library1.add_resource(self.book_of_list_type, 5)
+
+    def test_add_negative_number_of_books(self, setup_data):
+        """
+        Adding less than 1 book should throw a ValueError
+        """
+        with pytest.raises(ValueError):
+            self.library1.add_resource(self.book1, -1)
 
 
 if __name__ == '__main__':
