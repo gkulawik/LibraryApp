25552b3d8d71180bf185588c9b5176c3df90ffff
GKulawik <grzegorz.kulawik@rockwellautomation.com>
2024-03-15 14:50:17 +0100

    Create & backup the TODO list

--- src/main.py
+++ src/main.py
@@ -1,47 +1,49 @@
-from library import Library, Book, Dvd
-
+from library import Library, Book, Dvd, Cd, Magazine, Member, Memberships
 
 book1 = Book(title="The Lord of the Rings - Trilogy",
              genre="fantasy fiction",
-             availability_status=False,
              author="J.R.R. Tolkien",
              isbn="978-0261103252")
 
 book2 = Book(title="The hobbit, or There and back again",
              genre="fantasy fiction",
-             availability_status=False,
              author="J.R.R. Tolkien",
              isbn="978-0007458424")
 
 book3 = Book(title="Harry Potter Box Set: The Complete Collection",
              genre="fantasy fiction",
-             availability_status=False,
              author="J.K. Rowling",
              isbn="978-1408856772")
 
 book4 = Book(title="Deep Work: Rules for Focused Success in a Distracted World",
              genre="self-help",
-             availability_status=False,
              author="Cal Newport",
              isbn="978-0349411903")
 
 dvd1 = Dvd(title="Gladiator",
            genre="historical fiction",
-           availability_status=False,
            director="Ridley Scott",
            duration="2h 18min")
 
-library1 = Library()
-library1.add_resource(book1)
-library1.add_resource(dvd1)
-library1.add_resource(book2)
-library1.add_resource(book3)
-library1.add_resource(book4)
+library = Library()
+library.add_resource(book1, 2)
+library.add_resource(book2, 2)
+library.add_resource(book3, 2)
+member1 = library.create_member(name="John Doe", membership_status=Memberships.PREMIUM)
+print(member1.borrowed_resources_ids)
+member1.borrow_resources([1, 3])
+print(member1.borrowed_resources_ids)
+member1.borrow_resources([1, 3])
+print(member1.borrowed_resources_ids)
 
-print(library1.resources)
-print(library1.books)
+# todo:  Display available resources of each type (books, DVDs, CDs, magazines).
 
-# todo: Implement Pytest unit tests as a learning exercise
-# todo: Create an interface for creating new library resources?
+# todo:   Allow a member to return a resource.
 
+# todo: Display borrowed resources for a member. Pass borrowed IDs to the library
+#  and get back a list of borrowed resources with all details
 
+# todo: Display library statistics (total number of resources, number of available resources,
+#  number of borrowed resources).
+
+# todo: Write some basic unit tests or at least 1
